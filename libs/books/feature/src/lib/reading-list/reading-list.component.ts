50d4e54d23605da26c7e16d072745ca62cbbc14c
Muzeer Shaik <muzeer.shaik10@t-mobile.com>
2023-10-23 14:09:50 -0400

    Mark reading as finished feature

--- libs/books/feature/src/lib/reading-list/reading-list.component.ts
+++ libs/books/feature/src/lib/reading-list/reading-list.component.ts
@@ -1,18 +1,22 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { getReadingList, removeFromReadingList } from '@tmo/books/data-access';
+import { getReadingList, markBookAsFinished, removeFromReadingList } from '@tmo/books/data-access';
 
 @Component({
   selector: 'tmo-reading-list',
   templateUrl: './reading-list.component.html',
-  styleUrls: ['./reading-list.component.scss']
+  styleUrls: ['./reading-list.component.scss'],
 })
 export class ReadingListComponent {
-  public readingList$ = this._store.select(getReadingList);
+  readingList$ = this.store.select(getReadingList);
 
-  constructor(private readonly _store: Store) {}
+  constructor(private readonly store: Store) {}
 
-  public removeFromReadingList(item) {
-    this._store.dispatch(removeFromReadingList({ item }));
+  removeFromReadingList(item) {
+    this.store.dispatch(removeFromReadingList({ item }));
+  }
+
+  markAsFinished(item) {
+    this.store.dispatch(markBookAsFinished({ item  }));
   }
 }
